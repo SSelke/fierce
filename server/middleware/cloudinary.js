0f5658f3742d167f340a73010244053f7971c8dc
Scott Selke <pravaca22@gmail.com>
2019-05-17 20:16:05 -0700

    Added Image support

---
+++ server/middleware/cloudinary.js
@@ -0,0 +1,22 @@
+/* Config file for Cloudinary */
+
+const multer = require("multer");
+const cloudinary = require('cloudinary');
+const cloudinaryStorage = require("multer-storage-cloudinary");
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_KEY_SECRET
+});
+
+const storage = cloudinaryStorage({
+    cloudinary: cloudinary,
+    folder: 'products',
+    allowedFormats: ['jpg', 'png'],
+    transformation: [{width: 200, height: 200, crop: 'scale'}]
+});
+
+const parser = multer({storage});
+
+module.exports = parser;
